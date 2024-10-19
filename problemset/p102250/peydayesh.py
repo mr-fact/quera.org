188cc7e39a5d5b7119fa53e6e6c571b2c4e91314
mr-fact <mr.fact.202020@gmail.com>
2024-10-19 21:24:15 +0330

    Solved problem #/problemset/102250 - Score: 100/100

---
+++ problemset/p102250/peydayesh.py
@@ -0,0 +1,10 @@
+def find(num1, num2, num3):
+    if num1 != 1 and num2 != 1 and num3 != 1:
+        return 1
+    if num1 != 2 and num2 != 2 and num3 != 2:
+        return 2
+    if num1 != 3 and num2 != 3 and num3 != 3:
+        return 3
+    else:
+        return 4
+# print(find(1,2,3))
